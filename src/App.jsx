ac9b264a492fc5f06cc4b6d85a67d5668ec7a612
Prabakaran VM <prabakaran12vm@gmail.com>
2025-02-28 21:05:28 +0530

    igniting the app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    
+      <div className="bg-black">
+        <h1 className="text-3xl">Hello</h1>
+      </div>
+    
+  );
+}
+
+export default App;
