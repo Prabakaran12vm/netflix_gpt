7fabe035515e241ffc809cc0e29e8e07ab8ce970
Prabakaran VM <prabakaran12vm@gmail.com>
2025-03-21 23:36:39 +0530

    sign up logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,56 @@
 import React, { useRef } from "react";
 import { useState } from "react";
-import Header from './Header'
-import {checkValidData} from '../utils/vaidate'
-
+import Header from "./Header";
+import { checkValidData } from "../utils/vaidate";
+import { auth } from "../utils/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
-  
-  
-  const [errorMessage, setErrorMessage] = useState(null)
-  const email =useRef(null)
-  const password = useRef(null)
-  const name = useRef(null)
 
-  const handleButtonClick = ()=>{
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
+  const handleButtonClick = () => {
     // checkValidData
-    const message = checkValidData(email.current.value,password.current.value)
-    setErrorMessage(message)
-    
-    if(message) return;
-      
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
 
+    if (message) return;
 
-  }
+    // Sign In Sign Up logic
+    if (!isSignInForm) {
+      // Sign Up logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+          setErrorMessage(errorCode + " " + errorMessage);
+        });
+    } else {
+      // sign in logic
+    }
+  };
   const toggleSignInForm = () => {
-    setIsSignInForm(!isSignInForm)
-
-
-  }
+    setIsSignInForm(!isSignInForm);
+  };
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="absolute">
         <img
           className=""
@@ -40,31 +59,40 @@ const Login = () => {
         />
       </div>
       {/* Login form */}
-      <form onSubmit={(e)=>e.preventDefault()} className="w-3/12 absolute rounded-lg p-12  my-36  mx-auto right-0 left-0 text-white bg-black/70  ">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute rounded-lg p-12  my-36  mx-auto right-0 left-0 text-white bg-black/70  "
+      >
         <h1 className="font-bold  text-3xl py-4 ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         <input
-          ref ={email}
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700 m-2 h-[40px] "
         />
-        {!isSignInForm && <input
-          ref={name}
-          type="text"
-          placeholder="Full Name"
-          className="p-4 my-4 w-full bg-gray-700 m-2 h-[40px] "
-        />}
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 m-2 h-[40px] "
+          />
+        )}
         <input
-          ref ={password}
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 m-2 my-4 w-full  h-[40px] bg-gray-700 "
         />
         <p className="text-red-500 text-sm pl-2 ">{errorMessage}</p>
-        <button onClick={handleButtonClick} className=" m-4 ml-2 w-full text-white bg-red-700 h-[40px] rounded-lg">
-          {!isSignInForm ? "Sign In" : "Sign Up"}
+        <button
+          onClick={handleButtonClick}
+          
+          className=" m-4 ml-2 w-full text-white bg-red-700 h-[40px] rounded-lg"
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4" onClick={toggleSignInForm}>
           New to Netflix? Sign Up Now
